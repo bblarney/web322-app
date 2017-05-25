1edb4bae64f02dd574e4f959c54a67e766ffcd16
Brett Larney <you@example.com>
2017-05-25 13:12:19 -0400

    update server

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require("express");
 var path = require("path");
 var app = express();
 
+app.use(express.static('public'));
+
 var HTTP_PORT = process.env.PORT || 8080;
 
 // call this function after the http server starts listening for requests
@@ -11,12 +13,12 @@ function onHttpStart() {
 
 // setup a 'route' to listen on the default url path (http://localhost)
 app.get("/", function(req,res){
-   res.sendFile(path.join(__dirname + "/home.html"));
+   res.sendFile(path.join(__dirname + "/views/home.html"));
 });
 
 // setup another route to listen on /about
 app.get("/about", function(req,res){
-   res.sendFile(path.join(__dirname + "/about.html"));
+   res.sendFile(path.join(__dirname + "/views/about.html"));
 });
 
 // setup http server to listen on HTTP_PORT
