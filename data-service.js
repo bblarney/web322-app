8c7a20ac6cd6c09d9dc760a6d1d6019e2c5dea32
Brett Larney <you@example.com>
2017-06-08 15:54:36 -0400

    pretty much done

---
+++ data-service.js
@@ -0,0 +1,153 @@
+var fs= require('fs');
+
+var message = "";
+var employees = [];
+var departments = [];
+
+module.exports.setMessage = (msg) => {
+    return new Promise((resolve,reject)=>{
+        message = msg;
+        resolve();
+    });
+   
+};
+
+module.exports.getMessage = () => {
+    return new Promise((resolve,reject)=>{
+        if(message.length > 0){
+            resolve(message)
+        }else{
+            reject("Oh No!");
+        }
+    });
+}
+
+module.exports.initialize = () => {
+    var error;
+    return new Promise((resolve, reject) => {
+        fs.readFile('./data/employees.json', (err,data)=>{
+            if (err) {
+                reject(err);
+            }
+            employees = JSON.parse(data);
+            fs.readFile('./data/departments.json', (err,data)=>{
+                if (err) reject(err);
+                departments = JSON.parse(data);
+            });
+        });
+        resolve();
+    });
+}
+
+module.exports.getAllEmployees = () => {
+    return new Promise((resolve, reject) => {
+        if (employees.length == 0){
+            reject("no results returned");
+        }
+        else{
+            resolve(employees);
+        }
+    });
+}
+
+module.exports.getEmployeesByStatus = (status) => {
+    return new Promise((resolve, reject) => {
+        var tempEmployees = [];
+        var count = 0;
+        for (var i=0; i<employees.length; i++){
+            if (employees[i].status == status){
+                tempEmployees[count] = employees[i];
+                count++;
+            }
+        }
+
+        if (tempEmployees.length == 0){
+            reject("no results returned");
+        }
+        else{
+            resolve(tempEmployees);
+        }
+    });
+}
+
+module.exports.getEmployeesByDepartment = (department) => {
+    return new Promise((resolve, reject) => {
+        var tempEmployees = [];
+        var count = 0;
+        for (var i=0; i<employees.length; i++){
+            if (employees[i].department == department){
+                tempEmployees[count] = employees[i];
+                count++;
+            }
+        }
+
+        if (tempEmployees.length == 0){
+            reject("no results returned");
+        }
+        else{
+            resolve(tempEmployees);
+        }
+    });
+}
+
+module.exports.getEmployeesByManager = (manager) => {
+    return new Promise((resolve, reject) => {
+        var tempEmployees = [];
+        var count = 0;
+        for (var i=0; i<employees.length; i++){
+            if (employees[i].manager == manager){
+                tempEmployees[count] = employees[i];
+                count++;
+            }
+        }
+
+        if (tempEmployees.length == 0){
+            reject("no results returned");
+        }
+        else{
+            resolve(tempEmployees);
+        }
+    });
+}
+
+module.exports.getEmployeesByNum = (num) => {
+    return new Promise((resolve, reject) => {
+        for (var i=0; i<employees.length; i++){
+            if (employees[i].employeeNum == num){
+                resolve(employees[i]);
+            }
+        }
+        reject("no results returned");
+    });
+}
+
+module.exports.getManagers = () => {
+    return new Promise((resolve, reject) => {
+        var tempEmployees = [];
+        var count = 0;
+        for (var i=0; i<employees.length; i++){
+            if (employees[i].isManager == true){
+                tempEmployees[count] = employees[i];
+                count++;
+            }
+        }
+
+        if (tempEmployees.length == 0){
+            reject("no results returned");
+        }
+        else{
+            resolve(tempEmployees);
+        }
+    });
+}
+
+module.exports.getDepartments = () => {
+    return new Promise((resolve, reject) => {
+        if (departments.length != 0){
+            resolve(departments);
+        }
+        else{
+            reject("no results returned");
+        }
+    });
+}
--- server.js
+++ server.js
@@ -10,10 +10,11 @@
 *
 ********************************************************************************/
 
-
+var data_service = require("./data-service");
 var express = require("express");
 var path = require("path");
 var app = express();
+var fs = require("fs");
 
 app.use(express.static('public'));
 
@@ -25,14 +26,85 @@ function onHttpStart() {
 }
 
 // setup a 'route' to listen on the default url path (http://localhost)
-app.get("/", function(req,res){
+app.get("/", (req,res) => {
    res.sendFile(path.join(__dirname + "/views/home.html"));
 });
 
 // setup another route to listen on /about
-app.get("/about", function(req,res){
+app.get("/about", (req,res) =>{
    res.sendFile(path.join(__dirname + "/views/about.html"));
 });
 
+// setup another route to listen on /employees
+app.get("/employees", (req, res) => {
+
+    if(req.query.status){
+
+      data_service.getEmployeesByStatus(req.query.status).then((data) => {
+        res.json({data});
+        }).catch((err) => {
+          res.json({message: err});
+        });
+    }
+    else if(req.query.manager){
+        data_service.getEmployeesByManager(req.query.manager).then((data) => {
+        res.json({data});
+        }).catch((err) => {
+          res.json({message: err});
+        });
+    }
+    else if(req.query.department){
+        data_service.getEmployeesByDepartment(req.query.department).then((data) => {
+        res.json({data});
+        }).catch((err) => {
+          res.json({message: err});
+        });
+    }
+    else{
+        data_service.getAllEmployees().then((data)=>{
+            res.json({data});
+        }).catch((err)=>{
+            res.json({message: err});
+        });    
+    }
+});
+
+// setup another route to listen on /employees
+app.get("/employees/:empNum", (req,res) => {
+   data_service.getEmployeesByNum(req.params.empNum).then((data) => {
+    res.json({data});
+  }).catch((err) => {
+    res.json({message: err});
+  });
+});
+
+// setup another route to listen on /employees
+app.get("/managers", (req,res) => {
+   data_service.getManagers().then((data) => {
+    res.json({data});
+  }).catch((err) => {
+    res.json({message: err});
+  });
+});
+
+// setup another route to listen on /employees
+app.get("/departments", (req,res) => {
+  data_service.getDepartments().then((data) => {
+    res.json({data});
+  }).catch((err) => {
+    res.json({message: err});
+  });
+});
+
+//page not found
+app.use((req, res) => {
+  res.status(404).send("Page Not Found");
+});
+
 // setup http server to listen on HTTP_PORT
-app.listen(HTTP_PORT, onHttpStart);
+
+data_service.initialize().then(() => {
+  app.listen(HTTP_PORT, onHttpStart);
+}).catch((err) => {
+  console.log(err);
+});
